a00375c408b9414651edd6cf5f7640d7d78ab1a7
Mojtaba Rabiei <mojtaba.r127@gmail.com>
2016-12-15 00:18:01 +0330

    Add main method to find translation text

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const fs = require('fs');
 let langFilePath = './lang.json';
 
 // Current selected language, will be used to pick a correct translations
-let lang = 'default';
+let currentLang = 'default';
 
 // Keep all the translations on texts variable to have fast easy access
 let texts = null;
+
+// A flag to check if requested translation not exists return text with symbol
+// and output a console.error or not, just return original text
+let showError = true;
 
 /**
  * Read language.json file that's contain the list of all translations
@@ -52,3 +56,40 @@ readLangFile()
   .catch(err => {
     console.log(err.message);
   });
+
+function __(text, lang = currentLang) {
+  // If on default language then return the same text no need to continue
+  if (lang === 'default') {
+    return text;
+  }
+
+  let result = null;
+
+  // If texts load correctly then can access to it's fields
+  if (texts) {
+    // Get current text translations
+    let t = texts[text];
+
+    // If current text available then it's have all translations
+    if (typeof t !== 'undefined' && t) {
+      // Get selected language translation text
+      result = t[lang];
+    }
+  } else {
+    // If texts not available then it's on loading, return original text
+    console.error('Loading translation, please refresh your call!');
+    return text;
+  }
+
+  // If we have result then return it or return original text with error
+  if (result) {
+    return result;
+  } else {
+    if (showError) {
+      console.error(`The requested translation not found!\nOriginal text: ${text}\nRequested lang: ${lang}`);
+      return '__' + text + '__';
+    } else {
+      return text;
+    }
+  }
+}
